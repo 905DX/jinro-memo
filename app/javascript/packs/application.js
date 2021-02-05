72ed286b47f3f04be232a7a396e262827d2312bd
905DX <70510239+905DX@users.noreply.github.com>
2021-02-05 22:33:39 +0900

    セレクトボックスの複製機能の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,13 +8,11 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
-require('../articles')
 require('google/api_client')
 require('google/api_client/client_secrets')
 require('json')
 require('launchy')
 require('thin')
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
